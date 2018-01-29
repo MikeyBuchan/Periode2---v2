6bdff42488ce40b1a338bf84a3bc6f4127225360
MikeyBuchan <psmbgames@hotmail.com>
2018-01-29 23:58:15 +0100

    Added player health

---
+++ Game/Project 2/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour 
+{
+    public float health;
+    public string deathScene;
+
+
+    void OnCollisionEnter (Collision c)
+    {
+        if (c.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(deathScene);
+        }
+    }
+}
